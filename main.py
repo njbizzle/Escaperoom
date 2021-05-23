33957b48fb660a9b418a839bd4b640475266a0cb
njbizzle <nmurphy.mine@gmail.com>
2021-05-23 13:52:57 -0700

    test

---
+++ main.py
@@ -0,0 +1,5 @@
+#from gameloop import gameloopfunction
+#gameloopfunction()
+
+from story.bowling_alley.minigames.bowling_alley_bowling_game.bowling_alley_bowling_game_code import lowscore,medscore,highscore,guter,timeing_one_start
+timeing_one_start()
